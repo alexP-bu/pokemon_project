14fda3c2e8e9433f58a098043121ed8a401ab469
Alexander Pozin <apozin@bu.edu>
2019-11-26 02:25:43 +0000

    u

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -76,7 +76,7 @@ void Pokemon::StartMovingToGym(PokemonGym* gym){
 void Pokemon::StartTraining(unsigned int num_training_units){
 	if(isExhausted() == true){
 		cout << display_code << id_num << ": I am exhausted so no more training for me..." << endl;
-	}else if(!IN_GYM){
+	}else if(state != IN_GYM){
 		cout << display_code << id_num << ": I can only train in a Pokemon Gym!" << endl;
 	}else if(current_gym -> IsBeaten()){
 		cout << display_code << id_num << ": Cannot train! This Pokemon Gym is already Beaten!" << endl;
